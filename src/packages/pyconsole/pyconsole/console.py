2fdc89b64ee90c14ac89bc2618929abe21a9ceb8
Hugo Windisch <hugo.windisch@autodesk.com>
2021-06-04 16:21:53 -0400

    cosmetic code improvements

--- src/packages/pyconsole/pyconsole/console.py
+++ src/packages/pyconsole/pyconsole/console.py
@@ -1,11 +1,11 @@
 from qtmax import GetQMaxMainWindow
 from pyqtconsole.console import PythonConsole
 import pyqtconsole.highlighter as hl
-from PySide2.QtWidgets import QWidget, QMainWindow, QDockWidget, QToolButton, QToolBar, QAction
+from PySide2.QtWidgets import QWidget, QDockWidget, 
 from PySide2 import QtCore
 
 # My personal choice of colors
-HUGOS_THEME =  {
+HUGOS_THEME = {
     'keyword': hl.format('#7bd5d2', 'italic'),
     'operator': hl.format('#6ab6ba'),
     'brace': hl.format('#668799'),
@@ -19,7 +19,7 @@ HUGOS_THEME =  {
     'outprompt': hl.format('#d5d07b')
     }
 
-def new_console(tabto = None, floating = False, dockingarea = QtCore.Qt.RightDockWidgetArea):
+def new_console(tabto=None, floating=False, dockingarea=QtCore.Qt.RightDockWidgetArea):
     """
     Create a new console and float it as a max widget
     tabto: name of a widget on top of which the console should be tabbed
@@ -38,7 +38,7 @@ def new_console(tabto = None, floating = False, dockingarea = QtCore.Qt.RightDoc
     dock_widget.setObjectName("pyconsole")
     dock_widget.setWindowTitle("Python Console")
     main_window.addDockWidget(dockingarea, dock_widget)
-    if (not tabto is None):
+    if not tabto is None:
         tabw = main_window.findChild(QWidget, tabto)
         main_window.tabifyDockWidget(tabw, dock_widget)
     dock_widget.setFloating(floating)
